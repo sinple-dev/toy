5c106c0a3b5153cbec4e34747c4d9f3a551d5a3a
sinple <wlstjq11111@gmail.com>
2022-05-19 20:28:20 +0900

    jwt token ing~

--- auth/src/main/java/com/example/toy/auth/Application.java
+++ auth/src/main/java/com/example/toy/auth/Application.java
@@ -13,7 +13,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages ={"com.example.toy"})
 @PropertySource("classpath:application.properties")
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
