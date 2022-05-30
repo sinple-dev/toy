fa2388db6d8a31226e079ba9054663466a70b573
sinple <wlstjq11111@gmail.com>
2022-05-30 23:43:46 +0900

    batch

---
+++ batch/src/main/java/com/example/toy/batch/config/ExampleJobConfig.java
@@ -0,0 +1,43 @@
+package com.example.toy.batch.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+@EnableBatchProcessing
+public class ExampleJobConfig {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job ExampleJob(){
+        Job exampleJob = jobBuilderFactory.get("exampleJob")
+                .start(Step())
+                .build();
+        return exampleJob;
+    }
+
+    @Bean
+    public Step Step() {
+        return stepBuilderFactory.get("step")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("Step!");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+}
