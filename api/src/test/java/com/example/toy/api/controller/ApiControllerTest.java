f1834cb6403c23269cad5c4e1ec113eac93e0d69
sinple <wlstjq11111@gmail.com>
2022-07-23 20:18:05 +0900

    auth security add

--- api/src/test/java/com/example/toy/api/controller/ApiControllerTest.java
+++ api/src/test/java/com/example/toy/api/controller/ApiControllerTest.java
@@ -1,15 +1,11 @@
 package com.example.toy.api.controller;
 
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
