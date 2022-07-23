441a1785522fa5a7fc32b0f0b9d26dbd3621d2de
sinple <wlstjq11111@gmail.com>
2022-07-24 00:27:34 +0900

    auth security 정리

--- auth/src/main/java/com/example/toy/auth/config/SecurityConfig.java
+++ auth/src/main/java/com/example/toy/auth/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.example.toy.auth.config;
 
 import com.example.toy.auth.security.LoginFailureHandler;
 import com.example.toy.auth.security.LoginSuccessHandler;
-import com.example.toy.common.services.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,73 +19,49 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
-//위의 SecurityConfig에 붙은 @EnableWebSecurity을 보면 WebSecurityConfiguration.class,
-// SpringWebMvcImportSelector.class, OAuth2ImportSelector.class, HttpSecurityConfiguration.class들을 import해서 실행시켜주는 것을 알 수 있습니다.
-// 해당 annotation을 붙여야지 Securiry를 활성화 시킬 수 있습니다.
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private AuthenticationProvider authenticationProvider;
 
-    //로그인 성공시 이동할 Handler 의존성 주입
-    @Autowired
-    LoginSuccessHandler loginSuccessHandler;
-
-    @Autowired
-    LoginFailureHandler loginFailureHandler;
-
-    @Autowired
-    MyUserDetailsService myUserDetailsService;
-
-
-    //    /*
-//     * 스프링 시큐리티가 사용자를 인증하는 방법이 담긴 객체.
-//     */
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        /*
-//         * AuthenticationProvider 구현체
-//         */
-//        auth.authenticationProvider(authenticationProvider);
-////        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
-//    }
-//
-//
-//    /* * 스프링 시큐리티 룰을 무시하게 하는 Url 규칙(여기 등록하면 규칙 적용하지 않음) */
     @Override
-    public void configure(WebSecurity web) throws Exception {
-//        web.ignoring()
-//                .antMatchers("/resources/**", "/css/**", "/js/**", "/favicon*/**", "/img/**");
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/resources/**", "/css/**", "/js/**", "/favicon*/**", "/img/**");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .cors().configurationSource(corsConfigurationSource()).and()
-                .authorizeRequests() //보호된 리소스 URI에 접근할 수 있는 권한을 설정
-                .antMatchers("api/test").permitAll()
-                .antMatchers("/login").permitAll() //전체 접근 허용
-                .antMatchers("/error").permitAll()
-                .antMatchers("/logout").permitAll()
-                .antMatchers("/auth/signup").permitAll()
-                .antMatchers("/myPage").hasRole("ADMIN") //admin이라는 롤을 가진 사용자만 접근 허용
-                .antMatchers("/").permitAll()
-                .anyRequest().authenticated()
+                .cors().configurationSource(corsConfigurationSource())
+                .and()
+                    .authorizeRequests() //보호된 리소스 URI에 접근할 수 있는 권한을 설정
+                    .antMatchers("/api/test").permitAll()
+                    .antMatchers("/login").permitAll() //전체 접근 허용
+                    .antMatchers("/error").permitAll()
+                    .antMatchers("/logout").permitAll()
+                    .antMatchers("/auth/signup").permitAll()
+                    .antMatchers("/myPage").hasRole("ADMIN") //admin이라는 롤을 가진 사용자만 접근 허용
+                    .antMatchers("/").permitAll()
+                    .anyRequest().authenticated()
+                .and()
+
+                    .exceptionHandling()
+                    .authenticationEntryPoint() // 인증 예외처리
+                    .accessDeniedHandler() // 인가 예외처리
+
+                .and()
+
 
                 // 토큰을 활용하면 세션이 필요 없으므로 STATELESS로 설정하여 Session을 사용하지 않는다.
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-                .and()
-                .formLogin()
-                .usernameParameter("email")
-                .passwordParameter("password")
-                .successHandler(loginSuccessHandler)
-                .failureHandler(loginFailureHandler)
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
+//                .and()
+//                .formLogin()
+//                .usernameParameter("email")
+//                .passwordParameter("password")
+//                .successHandler(loginSuccessHandler)
+//                .failureHandler(loginFailureHandler)
+//                .permitAll()
 
     }
 
